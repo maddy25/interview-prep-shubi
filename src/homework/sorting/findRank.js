ae84fe2605d04478e3f52e5a23228b517189b9c8
Madhumita <chowdhury.madhumita1@gmail.com>
2018-08-02 23:17:54 -0400

    find rank

--- src/homework/sorting/findRank.js
+++ src/homework/sorting/findRank.js
@@ -7,13 +7,13 @@ function swap(a,lt,rt) {
     a[rt] = temp;
 }
 function partition(arr,st,en) {
-    let lt = st+1; let pt = st; let rt = en;
+    let lt = st+1; let pivot = arr[st]; let rt = en;
 
     while(lt<=rt) {
-        while(arr[lt] <= arr[pt]) {
+        while(arr[lt] <= pivot) {
             lt++;
         }
-        while(arr[rt]>arr[pt]){
+        while(arr[rt]>pivot){
             rt--;
         }
         if(lt<rt) {
@@ -21,34 +21,27 @@ function partition(arr,st,en) {
         }
     }
     swap(arr,st,rt);
+    // console.log(rt)
+    // console.log(arr);
     return rt;
 }
-function quickSort(arr,st,en,k,pI) {
+function quickSort(arr,st,en,k) {
+    if (st>en) {
+        return;
+    } else {
+        var pI = partition(arr,st,en);
+        if(arr[pI]<k) {
+            quickSort(arr,pI+1,en,k)
+        } else if(arr[pI]>k) {
+            quickSort(arr,st,pI-1,k)
+        } else {
+            console.log(pI+1);
+        }
 
-    if (st>=en) {
-        console.log(pI+1);
-        return pI+1;}
-    pI = partition(arr,st,en);
-    //quickSort(arr,st,pI-1,k)
-    //quickSort(arr,pI+1,en,k)
-    //console.log(arr)
-
-    if(arr[pI]<k) {
-        console.log(arr)
-        quickSort(arr,st,pI-1,k,pI)
-    }
-    if(arr[pI]>k) {
-        console.log(arr)
-        quickSort(arr,pI+1,en,k,pI)
-    } else if (arr[pI] === k) {
-        //console.log(pI)
-        return pI+1;
     }
 
-
+    // console.log(pI+1);
 
 }
-var testArr = [10,9,5,30]
-quickSort(testArr,0,testArr.length-1,5,0);
-
-
+var testArr = [3, 6, 10,9,5,30,11]
+quickSort(testArr,0,testArr.length-1,9);
